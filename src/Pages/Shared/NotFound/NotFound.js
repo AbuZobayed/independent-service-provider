89639e80297525f75c6769e935094497dacc2b44
AbuZobayed <zobayed.ict05@gmail.com>
2022-04-17 15:22:35 +0600

    services section done

--- src/Pages/Shared/NotFound/NotFound.js
+++ src/Pages/Shared/NotFound/NotFound.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import sleeping from '../../../images/sleeping.jpg'
+import notfount from '../../../images/404.jpg'
 
 const NotFound = () => {
     return (
         <div>
-            <h2 className='text-primary text-center'>Machanic is Sleeping</h2>
-            <img className='w-100' src={sleeping} alt="" />
+            
+            <img className='w-100' src={notfount} alt="" />
         </div>
     );
 };
